819b94248a9b2d41187d1bbfc7ef31e9ab0835c2
pedrof3 <pedro.dasilva.7080@gmail.com>
2023-03-17 23:18:19 -0300

    nova lógica de funcionamento iniciada

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-function insert(num) {
-  let numero = document.getElementById("saida").innerHTML;
-  document.getElementById("saida").innerHTML = `${numero + num}`;
+function number(num) {
+  if (document.getElementById("saida").innerHTML == `0`) {
+    document.getElementById("saida").innerHTML = num;
+  } else {
+    let numero = document.getElementById("saida").innerHTML;
+    document.getElementById("saida").innerHTML = `${numero + num}`;
+  }
+}
+
+function operator(ope) {
+  let screen = document.getElementById("saida").innerHTML
+  let rgex = /[0-9][^0-9$]/
+  if (screen == `0` || screen == `+` || screen == `-`) {
+    document.getElementById("saida").innerHTML = ope;
+  } else if (screen.match(rgex)) {
+    console.log('foi crl')
+  }
+  else {
+    document.getElementById("saida").innerHTML += ope
+  }
 }
 
 function limpar() {
-  document.getElementById("saida").innerHTML = ``;
+  document.getElementById("saida").innerHTML = `0`;
 }
 
 function calcular() {
