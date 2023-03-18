f33491601ca0fc4a520415f9c33c9425125cc493
pedrof3 <pedro.dasilva.7080@gmail.com>
2023-03-18 12:55:57 -0300

    finaliza lógica de funcionamento da calculadora

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Alterar todos os nomes declarados para português
+
 function number(num) {
   if (document.getElementById("saida").innerHTML == `0`) {
     document.getElementById("saida").innerHTML = num;
@@ -8,15 +10,24 @@ function number(num) {
 }
 
 function operator(ope) {
-  let screen = document.getElementById("saida").innerHTML
-  let rgex = /[0-9][^0-9$]/
+  let screen = document.getElementById("saida").innerHTML;
+  let regexOne = /[0-9]+[^0-9$]+[0-9]{1,}/gm;
+  let regexTwo = /[0-9]+[^0-9$]/gm;
   if (screen == `0` || screen == `+` || screen == `-`) {
-    document.getElementById("saida").innerHTML = ope;
-  } else if (screen.match(rgex)) {
-    console.log('foi crl')
-  }
-  else {
-    document.getElementById("saida").innerHTML += ope
+    ope == "+" || ope == "-"
+      ? (document.getElementById("saida").innerHTML = ope)
+      : console.log(`funcionando`);
+  } else if (screen.match(regexOne)) {
+    document.getElementById("saida").innerHTML = eval(screen);
+    document.getElementById("saida").innerHTML += ope;
+  } else if (screen.match(regexTwo)) {
+    document.getElementById("saida").innerHTML = screen.replace(
+      screen[screen.length - 1],
+      ope
+    );
+    // Controla possível erro de dois operadores juntos
+  } else {
+    document.getElementById("saida").innerHTML += ope;
   }
 }
 
@@ -26,8 +37,11 @@ function limpar() {
 
 function calcular() {
   let calcula = document.getElementById("saida").innerHTML;
+  let regexLNN = /[^0-9$]/;
 
-  if (calcula) {
+  if (calcula[calcula.length - 1].match(regexLNN)) {
+    document.getElementById("saida").innerHTML = calcula.slice(0, -1);
+  } else if (calcula) {
     document.getElementById("saida").innerHTML = eval(calcula);
   }
 }
